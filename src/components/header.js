4662d3a78e287e7235724f0ee04f1b63c83f0016
Kitart11 <klein1maria@gmail.com>
2018-11-09 18:57:18 -0500

    HeroGroup

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import './Header.css'
 
 const Header = ({ siteTitle }) => (
   <div className="Header">
     <div className="HeaderGroup">
-      <Link to="/"><img src="" width="30"/></Link>
+      <Link to="/"><img src={require('../images/logo-designcode.svg')} width="30" /></Link>
       <Link to="/courses">Courses</Link>
       <Link to="/downloads">Downloads</Link>
-      <Link to="/courses">Courses</Link>
-      <Link to="/courses">Courses</Link>
+      <Link to="/workshops">Workshops</Link>
+      <Link to="/buy"><button>Try for free</button></Link>
     </div>
   </div>
 )
