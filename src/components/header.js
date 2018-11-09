b0043a143fd8013f78a851e8666212f1067f1bcf
Kitart11 <klein1maria@gmail.com>
2018-11-09 12:29:04 -0500

    initial commit

---
+++ src/components/header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const Header = ({ siteTitle }) => (
+  <div className="Header">
+    <div className="HeaderGroup">
+      <Link to="/"><img src="" width="30"/></Link>
+      <Link to="/courses">Courses</Link>
+      <Link to="/downloads">Downloads</Link>
+      <Link to="/courses">Courses</Link>
+      <Link to="/courses">Courses</Link>
+    </div>
+  </div>
+)
+
+export default Header
