b7c5c205052fa080c1277702813b6f6fa7747f41
Kitart11 <klein1maria@gmail.com>
2018-11-10 22:32:02 -0500

    Cards

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import Card from '../components/Card';
 
 const IndexPage = () => (
   <div>
@@ -7,17 +8,17 @@ const IndexPage = () => (
       <div className="HeroGroup">
         <h1>Learn to <br />design and code <span>modern apps</span></h1>
         <p>Complete courses about the best tools and design systems.
-      Prototype and build apps with React and Swift.</p>
-        <Link to="/page-2/">Watch the video</Link>
+          Prototype and build apps with React and Swift.</p>
+          <Link to="/page-2/">Watch the video</Link>
             
 
         <div className="Logos">
-          <img src={require('../images/logo-sketch.png')} width="50"/>
-          <img src={require('../images/logo-figma.png')} width="50"/>
-          <img src={require('../images/logo-studio.png')} width="50"/>
-          <img src={require('../images/logo-framer.png')} width="50"/>
-          <img src={require('../images/logo-react.png')} width="50"/>
-          <img src={require('../images/logo-swift.png')} width="50"/>
+            <img src={require('../images/logo-sketch.png')} width="50"/>
+            <img src={require('../images/logo-figma.png')} width="50"/>
+            <img src={require('../images/logo-studio.png')} width="50"/>
+            <img src={require('../images/logo-framer.png')} width="50"/>
+            <img src={require('../images/logo-react.png')} width="50"/>
+            <img src={require('../images/logo-swift.png')} width="50"/>
         </div>
 
         <svg width="100%" height="172" fill="none" 
@@ -49,6 +50,10 @@ const IndexPage = () => (
           </path>  
         </svg>
       </div>
+    </div>
+    <div className="Cards">
+      <h2>11 courses, more coming</h2>
+      <Card />
     </div>
   </div>
 )
